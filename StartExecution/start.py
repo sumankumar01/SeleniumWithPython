1aae812a48231eeb20bb2b2738607cf557a474e7
Suman kumar <32861501+sumankumar01@users.noreply.github.com>
2020-04-25 01:25:20 +0530

    added the driver script

---
+++ StartExecution/start.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#driver = webdriver.Chrome(executable_path=r"C:\Users\kumar\AppData\Local\Programs\Python\Python38-32\Scripts\chromedriver.exe")
+
+driver=webdriver.Chrome()
+driver.get("http://www.newtours.demoaut.com/")
